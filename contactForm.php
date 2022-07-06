5deb740abdbfd5bac9e1a488b7f313922511533c
shivkinga53 <shivkinga53@gmail.com>
2022-07-06 13:14:50 +0530

    update

--- contactForm.php
+++ contactForm.php
@@ -8,7 +8,9 @@ if (isset($_POST['submit'])) {
     $message = $_POST['message'];
 
     $maiTo = "shivkinga53@gmail.com";
-    $headers = "From: ".$mailFrom;
+    $headers = "From: <".$mailFrom.">\r\n";
+    $txt = "You have received message from ".$name.".\n\n".$message;
 
-    mail($maiTo, $subject, $message);
+    mail($maiTo, $subject, $txt, $headers);
+    header("Location: index.php");
 }
